57648de45a5ec845a28100ec47a5b3e88617676c
mfarhan <mfarhan@mun.ca>
2025-10-13 01:14:57 -0400

    update svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,19 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
+  preprocess: vitePreprocess(),
 
-	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: '404.html'
-		})
-	}
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html'
+    }),
+    paths: {
+      base: '',
+    }
+  }
 };
 
 export default config;
